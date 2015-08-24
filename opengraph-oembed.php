73e2f239d85f93e4320ab48db3e6419496d988f2
Arūnas Liuiza <ask@arunas.co>
2015-08-24 04:54:01 +0300

    ability to rewrite css from theme

--- opengraph-oembed.php
+++ opengraph-oembed.php
@@ -17,8 +17,7 @@ class OpenGraph_oEmbed {
 	public static function init() {
 		$options = get_option( 'opengraph-oembed_options' );
 		self::$options = wp_parse_args( $options, self::$options );
-		self::$stylesheet = plugins_url( 'opengraph-oembed.css', __FILE__ );
-		self::$stylesheet = apply_filters( 'opengraph_oembed_stylesheet', self::$stylesheet );
+		self::$stylesheet = self::get_stylesheet();
 		add_action( 'wp_enqueue_scripts', 	array( 'OpenGraph_oEmbed', 'styles' ) );
 		add_action( 'admin_init', 			array( 'OpenGraph_oEmbed', 'admin_init' ) );
 		add_action( 'admin_menu', 			array( 'OpenGraph_oEmbed', 'admin_menu' ) );
@@ -29,28 +28,41 @@ class OpenGraph_oEmbed {
 			wp_embed_register_handler( $id, $regex, array( 'OpenGraph_oEmbed', 'oembed' ) );
 		}
 	}
-	private static function get_template( $url, $type = 'website' ) {
+	private static function _get_file( $url, $extension='php', $type = 'website', $method='dir' ) {
 		$plugin_dir = plugin_dir_path( __FILE__ );
+		$plugin_uri = 'dir' === $method ? $plugin_dir : plugins_url( '/', __FILE__ );
 		$theme_dir = get_template_directory() . '/';
+		$theme_uri = 'dir' === $method ? $theme_dir : ( get_template_directory_uri() . '/' );
 		$child_theme_dir = get_stylesheet_directory() . '/';
-		$template =  "{$plugin_dir}templates/opengraph-oembed.php";
-		if ( file_exists( "{$theme_dir}opengraph-oembed.php" ) ) {
-			$template = "{$theme_dir}opengraph-oembed.php";
+		$child_theme_uri = 'dir' === $method ? $child_theme_dir : ( get_stylesheet_directory_uri() . '/' );
+		$template =  "{$plugin_uri}templates/opengraph-oembed.{$extension}";
+		if ( file_exists( "{$theme_dir}opengraph-oembed.{$extension}" ) ) {
+			$template = "{$theme_uri}opengraph-oembed.{$extension}";
 		}
-		if ( file_exists( "{$child_theme_dir}opengraph-oembed.php" ) ) {
-			$template = "{$child_theme_dir}opengraph-oembed.php";
+		if ( file_exists( "{$child_theme_dir}opengraph-oembed.{$extension}" ) ) {
+			$template = "{$child_theme_uri}opengraph-oembed.{$extension}";
 		}
-		if ( file_exists( "{$plugin_dir}templates/opengraph-oembed-{$type}.php" ) ) {
-			$template = "{$plugin_dir}templates/opengraph-oembed-{$type}.php";
+		if ( file_exists( "{$plugin_dir}templates/opengraph-oembed-{$type}.{$extension}" ) ) {
+			$template = "{$plugin_uri}templates/opengraph-oembed-{$type}.{$extension}";
 		}
-		if ( file_exists( "{$theme_dir}opengraph-oembed-{$type}.php" ) ) {
-			$template = "{$theme_dir}opengraph-oembed-{$type}.php";
+		if ( file_exists( "{$theme_dir}opengraph-oembed-{$type}.{$extension}" ) ) {
+			$template = "{$theme_uri}opengraph-oembed-{$type}.{$extension}";
 		}
-		if ( file_exists( "{$child_theme_dir}opengraph-oembed-{$type}.php" ) ) {
-			$template = "{$child_theme_dir}opengraph-oembed-{$type}.php";
+		if ( file_exists( "{$child_theme_dir}opengraph-oembed-{$type}.{$extension}" ) ) {
+			$template = "{$child_theme_uri}opengraph-oembed-{$type}.{$extension}";
 		}
+		$template = apply_filters( 'opengraph_oembed_file', $template, $url, $extension );
+		return $template;
+	}
+	private static function get_template( $url, $type = 'website' ) {
+		$template = self::_get_file( $url, 'php', $type );
 		$template = apply_filters( 'opengraph_oembed_template', $template, $url );
 		$template = file_get_contents( $template );
+		return $template;
+	}
+	private static function get_stylesheet( ) {
+		$template = self::_get_file( false, 'css', '', 'uri' );
+		$template = apply_filters( 'opengraph_oembed_stylesheet', $template );
 		return $template;
 	}
 	public static function admin_init() {
