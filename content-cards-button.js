36ec179d02fe0cb4624939748b917ccb1856de10
Arūnas Liuiza <ask@arunas.co>
2015-09-16 15:26:06 +0300

    Advanced tab for shortcode modal dialog

--- content-cards-button.js
+++ content-cards-button.js
@@ -3,27 +3,52 @@
         editor.addButton( 'contentcards_shortcode', {
             image: contentcards.icon,
             onclick: function() {
-                editor.windowManager.open( {
+                var win = editor.windowManager.open( {
                     title: contentcards.texts.add_dialog_title,
+                    bodyType: 'tabpanel',
                     body: [{
-                        type: 'textbox',
-                        name: 'uri',
-                        label: contentcards.texts.link_label,
-                    },
-                    {
-                        type: 'checkbox',
-                        name: 'target',
-                        label: contentcards.texts.target_label,
-                        text: contentcards.texts.target_text,
-                        checked:false
-                    },
-                    {
-                        type: 'textbox',
-                        name: 'class',
-                        label: contentcards.texts.class_label,
+                        type: 'form',
+                        title: contentcards.texts.main_label,
+                        items: [{
+                            type: 'textbox',
+                            name: 'url',
+                            label: contentcards.texts.link_label,
+                        },
+                        {
+                            type: 'checkbox',
+                            name: 'target',
+                            label: contentcards.texts.target_label,
+                            text: contentcards.texts.target_text,
+                            checked:false
+                        }
+                        ],
+                    },{
+                        type: 'form',
+                        title: contentcards.texts.advanced_label,
+                        items: [{
+                            type: 'textbox',
+                            name: 'class',
+                            label: contentcards.texts.class_label,
+                        },{
+                            type: 'textbox',
+                            name: 'word_limit',
+                            label: contentcards.texts.wordlimit_label,
+                        }
+                        ],
                     }],
                     onsubmit: function( e ) {
-                        editor.insertContent( '[contentcards url="' + e.data.uri + '"' + ( e.data.target ? ' target="_blank"' : '' ) + ( e.data.class ? ' class="' + e.data.class + '"' : '' ) + '] ' );
+                        var result = win.toJSON();
+                        var atts = ''; 
+                        for ( var key in result ) {
+                            if ( !result[key] ) {
+                                continue;
+                            }
+                            if ( 'target' === key ) {
+                                result[key] = '_blank';
+                            }
+                            atts += ' ' + key  + '="' + result[key] + '"';
+                        }
+                        editor.insertContent( '[contentcards' + atts + '] ' );
                     }
                 } );
             }
